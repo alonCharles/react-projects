9e3c183531e21a8e43a2611dc5c482132a2dac5e
alonCharles <aloncharlesdev@gmail.com>
2023-11-01 12:49:42 -0400

    Update

--- social/client/src/App.js
+++ social/client/src/App.js
@@ -1,12 +1,13 @@
 //import Home from "./pages/home/Home";
 
-import Profile from "./pages/profile/Profile";
+import Login from "./pages/login/Login";
+//import Profile from "./pages/profile/Profile";
 
 function App() {
   return (
     <>
       {/* <Home /> */}
-      <Profile/>
+      <Login/>
     </>
   )
 }
